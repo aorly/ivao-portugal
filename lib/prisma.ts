2461af6461be2ca2990f9334aff1ac4f88b026dd
andre <andrevieirav6@gmail.com>
2026-01-15 23:31:32 +0000

    Added Maria DB support

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,19 +1,14 @@
 import { PrismaClient } from "@prisma/client";
-import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3";
 
 const globalForPrisma = global as unknown as {
   prisma?: PrismaClient;
 };
-
-const databaseUrl = process.env.DATABASE_URL ?? "file:./dev.db";
-const adapter = new PrismaBetterSqlite3({ url: databaseUrl });
 
 const canReuseClient = Boolean(globalForPrisma.prisma);
 
 export const prisma =
   (canReuseClient ? globalForPrisma.prisma : null) ??
   new PrismaClient({
-    adapter,
     log: process.env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
   });
 
