a9ac658edaf78ba1a9386ea709f5f91a365d9fc8
andre <andrevieirav6@gmail.com>
2026-01-15 22:17:00 +0000

    added plesk tools

---
+++ server.js
@@ -0,0 +1,19 @@
+const { createServer } = require("http");
+const next = require("next");
+
+const dev = process.env.NODE_ENV !== "production";
+const app = next({ dev });
+const handle = app.getRequestHandler();
+const port = Number(process.env.PORT || 3000);
+
+app
+  .prepare()
+  .then(() => {
+    createServer((req, res) => handle(req, res)).listen(port, () => {
+      console.log(`Server listening on port ${port}`);
+    });
+  })
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
